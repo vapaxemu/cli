dd2f860a44d64bae174ea7cb92eb52bed3eb5cf2
vapaxemu <vapaxemu@gmail.com>
2025-12-27 20:43:10 +0700

    Update _worker3.js

--- js/_worker3.js
+++ js/_worker3.js
@@ -20,8 +20,8 @@ const KV_PROXY_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/
 const PROXY_BANK_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 const DNS_SERVER_ADDRESS = "8.8.8.8";
 const DNS_SERVER_PORT = 53;
-const PROXY_HEALTH_CHECK_API = "https://id1.foolvpn.me/api/v1/check";
-const CONVERTER_URL = "https://api.foolvpn.me/convert";
+const PROXY_HEALTH_CHECK_API = "https://id1.foolvpn.web.id/api/v1/check";
+const CONVERTER_URL = "https://api.foolvpn.web.id/convert";
 const DONATE_LINK = "https://trakteer.id/dickymuliafiqri/tip";
 const BAD_WORDS_LIST =
   "https://gist.githubusercontent.com/adierebel/a69396d79b787b84d89b45002cb37cd6/raw/6df5f8728b18699496ad588b3953931078ab9cf1/kata-kasar.txt";
